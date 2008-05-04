d9dc5f61f84c8357b4c09e19eb05778e0ee6287a
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-05-04 11:18:36 +0000

    Arreglado para cumplir con pylint

--- trunk/core/bitutils.py
+++ trunk/core/bitutils.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python
 # coding: utf-8
+"""Utilidades para manipulación de bits en enteros."""
+
 from math import log
 
-# Fórmula mágica para encontrar el primer bit a 1 de un numero.
-# Se usa esto en lugar de la función ffsl de C por la longitud de los números
-# de 140 bits que son necesarios en el BITMAP_SIZE
+def ffs(i):
+    """Fórmula mágica para encontrar el primer bit a 1 de un numero.
+Se usa esto en lugar de la función ffsl de C por la longitud de los números
+de 140 bits que son necesarios en el BITMAP_SIZE."""
 
-def ffs(t):
-	try:
-		res=int(log(t & (~t+1) )/log(2))
-	except OverflowError:
-		res=-1
-	return res
+    try:
+        res = int(log(i & (~i+1)) / log(2))
+    except OverflowError:
+        res = -1
+    return res
 	
-# Establecer el bit siempre es posible.
-def set_bit(t, b):
-	return t + (1<<b)
-	
-# Si el bit está establecido, se limpia. Si no, no es necesario y se devuelve tal cual.
-def clear_bit(t, b):
-	if (t&(1<<b)):
-		ret= t - (1<<b)
-	else:
-		ret = t
-	return ret
+def set_bit(i, j):
+    """Establecer el bit siempre es posible."""
+
+    return i + (1<<j)
+
+def clear_bit(i, j):
+    """Si el bit está establecido, se limpia. 
+Si no, no es necesario y se devuelve tal cual."""	
+    if (i&(1<<j)):
+        ret = i - (1<<j)
+    else:
+        ret = i
+    return ret
 
 if __name__ == "__main__":
-	from sys import argv
-	if len(argv) != 2:
-		print "Uso: %s numero" % argv[0]
-	else:
-		print ffs(long(argv[1]))
+    from sys import argv
+    if len(argv) != 2:
+        print "Uso: %s numero" % argv[0]
+    else:
+        print ffs(long(argv[1]))
