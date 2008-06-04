d9317a3f19a038fa0a954256410ca0ccf5d1a63e
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-06-04 22:49:21 +0000

    Iniciada la emulación con valores estáticos. Añadidas varias tareas de pruebas

--- trunk/core/runqueue.py
+++ trunk/core/runqueue.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from prio_array import prio_array
+import const
 
 class runqueue():
     def __init__(self, CPU):
@@ -8,5 +9,18 @@ class runqueue():
         self.cpu = CPU
         print "Iniciando runqueue de la CPU", self.cpu.id
         self.active = prio_array()
+        self.active.name="Active" # Para depurar
         self.expired = prio_array()
+        self.expired.name="Expired" # Para depurar
+        self.expired_timestamp = 0
+        self.nr_running = 0
+        self.current = None
+        self.best_expired_prio = 0
+        
         print "Priority Arrays creados"
+        
+    def starving(self):
+        s = const.STARVATION_LIMIT and self.expired_timestamp \
+        and (cpu.clock - self.expired_timestamp >=  consts.STARVATION_LIMIT * 
+        this.nr_running + 1) or (this.current.static_prio > this.best_expired_prio)
+        return s
