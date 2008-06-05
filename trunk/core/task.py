564c0f50ff5e658399976f18ef96a3346e445b5f
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-06-05 21:52:47 +0000

    Implementadas la mayor parte de las funciones del scheduler real.

--- trunk/core/task.py
+++ trunk/core/task.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 """ Clase para representar tareas del planificador"""
-import const
+from const import *
 from time import sleep
 
 class task():
@@ -19,21 +19,22 @@ class task():
         self.timeline = {}
         self.name = ""
         self.localtime = -1
-        self.state = None
-        self.prio = 0
+        self.state = state["RUNNING"]
+        self.oldstate = self.state
         self.static_prio = 0
+        self.prio = 0
         self.run_list = None
         self.array = None 
-        self.sleep_avg = None
-        self.timestamp = None
-        self.last_ran = None
-        self.activated = None
-        self.policy = const.policy["NORMAL"]
+        self.sleep_avg = 0
+        self.timestamp = 0
+        self.last_ran = 0
+        self.activated = 0
+        self.policy = policy["NORMAL"]
         # En sched_fork() se asigna la mitad del timeslice del proceso padre, 
         # del que carecemos, por lo que uso un time_slice por defecto.
         self.time_slice = 100
         self.first_time_slice = 1
-        self.rt_priority = None
+        self.rt_priority = 0
         self.flags = 0
         
         try: 
@@ -42,15 +43,15 @@ class task():
             print "Error al procesar la tarea de fuente %s" % source
             return None
 
-    def __str__(self):
-        """ Representación en texto de una tarea"""
-        tmp =""
-        tmp += "Nombre: %s\n" % self.name
-        tmp += "Prioridad: %d\n" % self.static_prio
-        tmp += "Clase: %d\n" % self.policy
-        tmp += "Linea temporal: %s\n" % self.timeline
-        tmp += "Estado: %d\n" % self.state
-        return tmp
+#    def __str__(self):
+#        """ Representación en texto de una tarea"""
+#        tmp =""
+#        tmp += "Nombre: %s\n" % self.name
+#        tmp += "Prioridad: %d\n" % self.static_prio
+#        tmp += "Clase: %d\n" % self.policy
+#        tmp += "Linea temporal: %s\n" % self.timeline
+#        tmp += "Estado: %d\n" % self.state
+#        return tmp
             
     def _fill_timeline(self, source):
         """Genera una línea de tiempo de ejecución del proceso a partir del
@@ -65,7 +66,7 @@ class task():
             elif tmp[0] == "PRIO":
                 self.static_prio = int(tmp[1].strip())
             elif tmp[0] == "CLASS":
-                self.policy = const.policy[tmp[1].strip()]
+                self.policy = policy[tmp[1].strip()]
             else:
                 when = int(tmp[0])
                 what = tmp[1].strip()
@@ -73,42 +74,111 @@ class task():
 
     def update_state(self):
         """ Comprueba el estado del proceso y lo actualiza si ha cambiado"""
-        if self.localtime in self.timeline and const.state[self.timeline[self.localtime]]  != self.state:
+        if self.localtime in self.timeline and state[self.timeline[self.localtime]] != self.state:
             self.oldstate = self.state
-            self.state = const.state[self.timeline[self.localtime]]
-            self.flags = const.NEED_RESCHED
+            self.state = state[self.timeline[self.localtime]]
+            self.flags = NEED_RESCHED
             print "%s[%d]: Cambio de estado: %s -> %s" % (self.name, self.localtime, self.oldstate, self.state)
         self.flags = None
         
     def tick(self):
         self.update_state()
-        self.localtime+=1
+        # Solo actualizamos la hora para saber si pasamos a estar activos.
+        # Si actualizara también en RUNNING, parecería que hemos tenido la CPU
+        # sin tenerla, pues no somos "current"
+        if self.state != state["RUNNING"]:
+            self.localtime+=1
         
     def task_timeslice(self):
-        ## TODO: Rellenar esta funcion
-        return 100
+        if self.static_prio < 120:
+            x = DEF_TIMESLICE*4
+        else:
+            x = DEF_TIMESLICE
+        ret = max ( x*(MAX_PRIO-self.prio) / (MAX_USER_PRIO/2), MIN_TIMESLICE)
+        return ret
     
     def effective_prio(self):
-        ## TODO Devuelve la prioridad efectiva.
-        return 100
+        # Devuelve la prioridad efectiva.
+        bonus = self.current_bonus() - MAX_BONUS/2
+        return self.static_prio - bonus
+    
+    def current_bonus(self):
+        ret = self.sleep_avg * MAX_BONUS / MAX_SLEEP_AVG
+        return ret
     
     def interactive(self):
         # TODO: Indica si la tarea es interactiva
-        return False
+        return self.prio <= (self.static_prio - self.delta())
+    
+    def interactive_sleep(self):
+    #58#define PRIO_TO_NICE(prio)      ((prio) - MAX_RT_PRIO - 20)
+    #59#define TASK_NICE(p)            PRIO_TO_NICE((p)->static_prio)
+    #91#define INTERACTIVE_DELTA         2
+    #139#define SCALE(v1,v1_max,v2_max) \
+    #140        (v1) * (v2_max) / (v1_max)
+    #141
+    #142#define DELTA(p) \
+    #143        (SCALE(TASK_NICE(p), 40, MAX_BONUS) + INTERACTIVE_DELTA)
+    #148#define INTERACTIVE_SLEEP(p) \
+    #149        (JIFFIES_TO_NS(MAX_SLEEP_AVG * \
+    #150                (MAX_BONUS / 2 + DELTA((p)) + 1) / MAX_BONUS - 1))
+       ret = (MAX_SLEEP_AVG * (MAX_BONUS/2 + self.delta() +1) / MAX_BONUS - 1)
+       return ret
+   
+    def delta(self):
+        # 58#define PRIO_TO_NICE(prio)      ((prio) - MAX_RT_PRIO - 20)
+        # 59#define TASK_NICE(p)            PRIO_TO_NICE((p)->static_prio)
+        # 139#define SCALE(v1,v1_max,v2_max) \
+        # 140        (v1) * (v2_max) / (v1_max)
+        # 141
+        # 142#define DELTA(p) \
+        # 143        (SCALE(TASK_NICE(p), 40, MAX_BONUS) + INTERACTIVE_DELTA)
+        tn = self.static_prio - MAX_RT_PRIO - 20
+        escala = tn * MAX_BONUS / 40
 
-    def recalc_task_prio(self):
+        return escala + INTERACTIVE_DELTA
+    
+    def recalc_task_prio(self, now):
         # TODO: Rellenar esta función :P
-        self.prio = self.static_prio
+        if now - self.timestamp > MAX_SLEEP_AVG:
+            self.time_slice = MAX_SLEEP_AVG
+        else:
+            self.time_slice = now - self.timestamp
 
+        if sleep_time:
+            if self.activated == -1 and sleep_time > self.interactive_sleep():
+                p.sleep_avg = MAX_SLEEP_AVG - DEF_TIMESLICE
+            else:
+                sleep_time *= max(MAX_BONUS - self.current_bonus(), 1)
+                if self.activated == -1:
+                    if self.sleep_avg >= self.interactive_sleep():
+                        self.sleep_avg = self.interactive_sleep()
+                        sleep_time = 0
+                
+                self.sleep_avg+=sleep_time
+                
+                if self.sleep_avg > MAX_SLEEP_AVG:
+                    self.sleep_avg = MAX_SLEEP_AVG
+        
+        self.prio = self.effective_prio()
+                    
 
+    def timeslice_granularity(self):
+    #135#define TIMESLICE_GRANULARITY(p) (GRANULARITY * \
+    #136                (1 << (((MAX_BONUS - CURRENT_BONUS(p)) ? : 1) - 1)))
+        ret = max( GRANULARITY 
+            * (1 << (MAX_BONUS - self.current_bonus())), 1) -1
+        return ret
+
+        
 if __name__ == "__main__":
     tmp = task("tasks/task1.tsk")
     print tmp
     
     print "Demo de ejecución y cambio de estado"
-    while tmp.state != const.state["EXIT"]:
+    while tmp.state != state["EXIT"]:
         print "Tick: ", tmp.localtime
         tmp.tick()
-        sleep(0.1/const.HZ)
+        sleep(0.1/HZ)
 
         
