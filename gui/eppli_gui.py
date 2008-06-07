11a614bef1ead67c2528433dd5ddeba8ca66fc80
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-06-07 22:47:23 +0000

    Esqueleto del controlador y la vista

--- gui/eppli_gui.py
+++ gui/eppli_gui.py
@@ -15,19 +15,60 @@ class eppli_gui():
                         "on_boton_nuevo_clicked": self.select_tasks_dir,
                         "on_boton_start_pause_toggled": self.run_pause}
         
-        self.appXML = gtk.glade.XML("eppli.glade")
+        self.appXML = gtk.glade.XML("gui/eppli.glade")
         self.appXML.signal_autoconnect(self.eventos)
         
-        sel.eppliWindow = self.appXML.get_widget("mainWindow")
+        self.eppliWindow = self.appXML.get_widget("mainWindow")
         
         self.controller = eppli_controller(self)
         
     def run(self):
-        self.update_all()
-        self.appXML.show()
+        """ Inicia el GUI y el bucle principal de gtk."""
+        #self.update_all()
+        self.eppliWindow.show()
+        gtk.main()
     
-    def update_all(self):
+    def _update_all(self):
+        """ Actualiza todos los datos en pantalla cada vez que se completa 
+        una iteración del planificador. Esta función es llamada por el 
+        controlador."""
         raise NotImplemented()
+    
+    def __update_stats_current(self):
+        """ Actualiza las estadísticas del proceso current."""
+        raise NotImplemented()
+
+    def __update_stats_selected(self):
+        """ Actualiza las estadísticas del proceso seleccionado."""
+        raise NotImplemented()
+    
+    def __update_stats_scheduler(self):
+        """ Actualiza las estadísticas del planificador."""
+        raise NotImplemented()
+
+    def __update_bitmap_active(self):
+        """ Actualiza la imagen del bitmap active y el número de procesos
+        que tiene asignados."""
+        raise NotImplemented()
+    
+    def __update_bitmap_expired(self):
+        """ Actualiza la imagen del bitmap expired y el número de procesos
+        que tiene asignados."""
+        raise NotImplemented()
+   
+    def __update_bitmap(self, drawable, bits):
+       """ Actualiza la imagen de drawable con los valores pasados en bits"""
+       raise NotImplemented()
+    
+    def run_pause(self, widget):
+        """ Ejecuta y pausa la ejecución del planificador.
+        También alterna el aspecto del botón para reflejar la pausa."""
+        raise NotImplemented()
+    
+    def select_tasks_dir(self, widget):
+        """ Abre un cuadro de diálogo para seleccionar el directorio que 
+        contiene las tareas a emular."""
+        raise NotImplemented() 
         
 if __name__ == "__main__":
     e = eppli_gui()
