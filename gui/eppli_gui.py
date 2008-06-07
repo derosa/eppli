fde25ec4ffa00c1b94731e253fc08646c9e3037a
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-06-07 22:23:29 +0000

    Trabajo inicial en el controlador del gui

--- gui/eppli_gui.py
+++ gui/eppli_gui.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python
 #coding: utf-8
 
-from core.sched import scheduler
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtk.glade
 
-s = scheduler("../core/tasks")
-s.run()
+from eppli_controller import eppli_controller
+from eppli_controller import NotImplemented
 
+class eppli_gui():
+    def __init__(self):
+        self.eventos = { "on_mainWindow_destroy": gtk.main_quit,
+                        "on_boton_nuevo_clicked": self.select_tasks_dir,
+                        "on_boton_start_pause_toggled": self.run_pause}
+        
+        self.appXML = gtk.glade.XML("eppli.glade")
+        self.appXML.signal_autoconnect(self.eventos)
+        
+        sel.eppliWindow = self.appXML.get_widget("mainWindow")
+        
+        self.controller = eppli_controller(self)
+        
+    def run(self):
+        self.update_all()
+        self.appXML.show()
+    
+    def update_all(self):
+        raise NotImplemented()
+        
+if __name__ == "__main__":
+    e = eppli_gui()
+    e.run()
