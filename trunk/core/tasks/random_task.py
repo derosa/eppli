6708e3ae809f0e3c99a784d9adfde1b600d7ebe5
soulnet <soulnet@4ffa2f65-bd48-0410-a2a6-7decf0395462>
2008-06-08 22:45:57 +0000

    Menor rango para procesos más cortos

--- trunk/core/tasks/random_task.py
+++ trunk/core/tasks/random_task.py
@@ -8,7 +8,7 @@ from random import randrange
 state = {0: "INTERRUPTIBLE", 1: "UNINTERRUPTIBLE", 2: "RUNNING", 3: "EXIT"}
 salida={}
 
-for t in range(1, 100):
+for t in range(1, 30):
 	when = randrange(1, 10)*t
 	what = state[randrange(3)]
 	salida[when] = what
@@ -24,5 +24,5 @@ print "PRIO=120"
 for v in valores:
 	print "%s=%s" % (v,salida[v])
 	
-print "1000=RUNNING"
-print "1001=EXIT"
+print "301=RUNNING"
+print "302=EXIT"
